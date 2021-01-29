c967c91af18039a34d055be7622c0658c2d2c54f
chuyi <chuyingl@umich.edu>
2021-01-29 10:17:13 -0800

    add folder

---
+++ live_stock_app/live_stock_ui/vendor.js
@@ -0,0 +1,8 @@
+import 'react';
+import 'react-dom';
+
+//Third party packages
+import 'jquery';
+import 'popper.js';
+import 'bootstrap/dist/js/bootstrap';
+import 'bootswatch/dist/united/bootstrap.min.css';
